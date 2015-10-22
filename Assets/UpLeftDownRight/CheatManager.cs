cf0c17c0574c3dbf89a77290bab9bee08fc6f51a
Shane O'Brien <theshaneobrien@gmail.com>
2015-10-22 15:50:09 +0100

    Initial Concept

---
+++ Assets/UpLeftDownRight/CheatManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class CheatManager : MonoBehaviour {
+
+	public List<string> lastInputs = new List<string>();
+
+	public List<List<string>> cheatCodes = new List<List<string>>();
+
+	public int cheatCodeLength;
+
+	// Use this for initialization
+	void Start () {
+
+		List<string> code1 = new List<string>();
+		code1.Add("A");
+		code1.Add("B");
+		code1.Add("C");
+		code1.Add("D");
+		code1.Add("E");
+		code1.Add("F");
+		code1.Add("G");
+		code1.Add("H");
+		code1.Add("I");
+		code1.Add("J");
+
+		cheatCodes.Add(code1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+		{
+			if (Input.GetKeyDown(key))
+			{
+				lastInputs.Add(key.ToString());
+				foreach (List<string> cheatCode in cheatCodes)
+				{
+					if (Enumerable.SequenceEqual(lastInputs.OrderBy(t => t), cheatCode.OrderBy(t => t)))
+					{
+						Debug.Log("We Got a match!");
+						lastInputs.Clear();
+					}
+				}
+				
+			}
+		}
+	}
+}
